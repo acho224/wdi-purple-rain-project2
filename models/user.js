2799c1f6919b79eee9799891bdd4d5f1f484bc9b
Arlen Cho <arlen_cho@hotmail.com>
2016-07-05 09:56:47 -0400

    add db connection for heroku

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const dbConnection = 'mongodb://localhost:27017/playpal';
+const dbConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/playpal';
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSalt(10);
 
